c34ee001c75123ec52a6150e36f7970819c70195
KomaCrazy <zxloveninek90@gmail.com>
2024-12-12 20:28:22 +0700

    xxx

--- basic/api/index.js
+++ basic/api/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js API!');
+  res.send('Hello World from Node.123123js API!');
 });
 
 app.listen(port, () => {
